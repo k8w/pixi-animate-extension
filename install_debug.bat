e5a067211357de28acca99b1a8cc063418975c28
k8w <twoeo@qq.com>
2017-12-18 23:55:54 +0800

    install_debug.bat

---
+++ install_debug.bat
@@ -0,0 +1,6 @@
+rem 获取管理员权限
+mode con lines=20 cols=60
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+
+cd /d "%~dp0"
+gulp --debug --install && pause
